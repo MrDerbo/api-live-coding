2565c16442b14a56fa835c56029033a704ace15b
MrDerbo <kkarimovl2009@gmail.com>
2025-04-20 18:12:44 +0300

    Создает корневой модуль

---
+++ index.js
@@ -0,0 +1,167 @@
+let tasks = [];
+    const host = 'https://webdev-hw-api.vercel.app/api/v2/todos';
+    let token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k';
+    token = null;
+
+    const fetchTodosAndRender = () => {
+      return fetch(host, {
+        method: "GET",
+        headers: {
+            Authorization: token,
+        }
+      })
+        .then((response) => {
+            if (response.status === 401) {
+                throw new Error('Нет авторизации')
+            }
+          return response.json();
+        })
+        .then((responseData) => {
+          tasks = responseData.todos;
+          renderApp();
+        });
+    };
+
+    const renderApp= () => {
+      const appEl = document.getElementById('app');
+
+      if (!token) {
+        const appHtml = `
+        <h1>Список задач</h1>
+      <div class="form">
+        <h3 class="form-title">Вход</h3>
+        <div class="form-row">
+          Логин:
+          <input
+            type="text"
+            id="login-input"
+            class="input"
+          />
+          <br />
+          Пароль:
+          <input
+            type="text"
+            id="login-input"
+            class="input"
+          />
+        </div>
+        <br />
+        <button class="button" id="login-button">Войти</button>
+      </div>
+      `;
+
+      appEl.innerHTML = appHtml;
+
+      const logBtn = document.getElementById('login-button');
+      logBtn.addEventListener('click', () => {
+        token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k';
+        fetchTodosAndRender();
+      });
+
+      return;
+    }
+      const tasksHtml = tasks
+        .map((task) => {
+          return `
+          <li class="task">
+            <p class="task-text">
+              ${task.text}
+              <button data-id="${task.id}" class="button delete-button">Удалить</button>
+            </p>
+          </li>`;
+        })
+        .join("");
+      
+      const appHtml = `
+        <h1>Список задач</h1>
+      <ul class="tasks" id="list">
+        ${tasksHtml}
+      </ul>
+      <br />
+      <div class="form">
+        <h3 class="form-title">Форма добавления</h3>
+        <div class="form-row">
+          Что нужно сделать:
+          <input
+            type="text"
+            id="text-input"
+            class="input"
+            placeholder="Выпить кофе"
+          />
+        </div>
+        <br />
+        <button class="button" id="add-button">Добавить</button>
+      </div>
+      `;
+
+      appEl.innerHTML = appHtml;
+
+      // Назначаем обработчики событий после рендеринга
+      const deleteButtons = document.querySelectorAll(".delete-button");
+      const addButton = document.getElementById("add-button");
+      const textInput = document.getElementById("text-input");
+
+      for (const deleteButton of deleteButtons) {
+        deleteButton.addEventListener("click", (event) => {
+          event.stopPropagation();
+
+          const id = deleteButton.dataset.id;
+
+          fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
+            method: "DELETE",
+            headers: {
+                Authorization: token,
+            }
+          })
+            .then((response) => {
+              return response.json();
+            })
+            .then((responseData) => {
+              tasks = responseData.todos;
+              renderApp();
+            });
+        });
+      }
+
+      addButton.addEventListener("click", () => {
+        if (textInput.value === "") {
+          return;
+        }
+
+        addButton.disabled = true;
+        addButton.textContent = "Задача добавляется...";
+
+        fetch(host, {
+          method: "POST",
+          body: JSON.stringify({
+            text: textInput.value,
+          }),
+          headers: {
+              Authorization: token,
+          },
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then(() => {
+            textInput.value = "";
+          })
+          .then(() => {
+            return fetchTodosAndRender();
+          })
+          .then(() => {
+            addButton.disabled = false;
+            addButton.textContent = "Добавить";
+          })
+          .catch((error) => {
+            console.error(error);
+            alert("Кажется, у вас проблемы с интернетом, попробуйте позже");
+            addButton.disabled = false;
+            addButton.textContent = "Добавить";
+          });
+      });
+    };
+
+    // Инициализация приложения
+    // fetchTodosAndRender();
+    renderApp();
