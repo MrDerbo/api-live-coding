8db076f0c2da90f1414bfe47b550cd508b5937ff
MrDerbo <kkarimovl2009@gmail.com>
2025-04-20 18:58:34 +0300

    Выносит форму входа в отдельный компонент

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { deleteTodos, getTodos, postTodos } from "./api.js";
+import { loginFinction } from "./components/login.js";
  
 let tasks = [];
     const host = 'https://webdev-hw-api.vercel.app/api/v2/todos';
@@ -17,40 +18,13 @@ let tasks = [];
       const appEl = document.getElementById('app');
 
       if (!token) {
-        const appHtml = `
-        <h1>Список задач</h1>
-      <div class="form">
-        <h3 class="form-title">Вход</h3>
-        <div class="form-row">
-          Логин:
-          <input
-            type="text"
-            id="login-input"
-            class="input"
-          />
-          <br />
-          Пароль:
-          <input
-            type="text"
-            id="login-input"
-            class="input"
-          />
-        </div>
-        <br />
-        <button class="button" id="login-button">Войти</button>
-      </div>
-      `;
-
-      appEl.innerHTML = appHtml;
-
-      const logBtn = document.getElementById('login-button');
-      logBtn.addEventListener('click', () => {
-        token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k';
-        fetchTodosAndRender();
-      });
-
+      loginFinction({ appEl, 
+        setToken: (newToken) => {
+         token = newToken;
+      }, fetchTodosAndRender })
       return;
     }
+    
       const tasksHtml = tasks
         .map((task) => {
           return `
@@ -137,6 +111,4 @@ let tasks = [];
       });
     };
 
-    // Инициализация приложения
-    // fetchTodosAndRender();
     renderApp();
